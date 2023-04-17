f5b3f5e796efa92b50670fb453cb7039abd98f05
FlavioLionelRita <flaviolrital@hotmail.com>
2023-04-17 03:08:20 +0200

    replace json-light for typ3s

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,7 +32,7 @@ module.exports = function (grunt) {
 		}
 		data.main = 'index.js'
 		data.types = 'index.d.ts'
-		data.bin = { 'json-light': 'infrastructure/cli/json-light.js' }
+		data.bin = { 'typ3s': 'infrastructure/cli/typ3s.js' }
 		fs.writeFileSync('dist/package.json', JSON.stringify(data, null, 2), 'utf8')
 	})
 	grunt.registerTask('dist', ['clean:dist', 'clean:build', 'exec:tsc', 'exec:lint', 'exec:test', 'copy:lib', 'copy:bin', 'copy:jest', 'copy:readme', 'copy:license', 'create-package'])
--- bug_report.md
+++ bug_report.md
@@ -2,7 +2,7 @@
 name: Bug report
 ---
 (**NOTE**: This is used to report product bugs:
-  To report a security vulnerability, please visit [issues](https://github.com/FlavioLionelRita/json-light/issues)
+  To report a security vulnerability, please visit [issues](https://github.com/FlavioLionelRita/typ3s/issues)
  
 **Bug description**
 
