cb1e81ff92c1bec4b96fa6fbfcad6bb8f4a55ea1
FlavioLionelRita <flaviolrital@hotmail.com>
2024-01-16 06:53:30 +0100

    feat: new version

--- src/dev/labs/country.ts
+++ src/dev/labs/country.ts
@@ -7,7 +7,7 @@ import { Type } from '../../lib'
 		phoneCode: '34',
 		timezones: [
 			{ name: 'Madrid', offset: 1, pos: { lat: 40.4165, log: -3.70256 } },
-			{ name: 'Ceuta', offset: 1, pos: { lat: 35.8883, log: -5.3162 } },
+			{ name: 'Ceuta', pos: { lat: 35.8883, log: -5.3162 } },
 			{ name: 'Canary', offset: 0, pos: { lat: 28.1248, log: -15.43 } }
 		]
 	}
